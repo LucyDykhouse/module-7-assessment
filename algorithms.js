a36e6c2fd7f70bdda77b0d094197f8098c0aaef7
Lucy Dykhouse <l.dykhouse24@gmail.com>
2021-11-28 21:13:25 -0500

    Finished hasUniqueChars

--- algorithms.js
+++ algorithms.js
@@ -13,14 +13,45 @@ function addToZero(arr) {
 }
 
 // Testing arrays
+console.log(`\n------- addToZero -------`);
 console.log(addToZero([]));
 console.log(addToZero([1]));
 console.log(addToZero([1, 2, 3]));
 console.log(addToZero([1, 2, 3, -2]));
 
 // Time complexity
-// O(n)
+// O(n^2)
 
 // Space complexity
 // O(n)
+
+
+
+// Question 2: has unique characters
+
+function hasUniqueChars(str) {
+    let wordArray = str.split('');
+    let comparisonChar = wordArray[0];
+    for (let i = 1; i < wordArray.length; i++) {
+        if (wordArray[i] === comparisonChar) {
+          return false;
+        } else {
+          comparisonChar = wordArray[i];
+        }
+    }
+    return true;
+}
+
+// Testing strings
+console.log(`\n------- hasUniqueChars -------`);
+console.log(hasUniqueChars("Monday"));
+console.log(hasUniqueChars("Moonday"));
+
+// Time complexity
+// O(n^2)
+
+// Space complexity
+// O(n)
+
+
 
